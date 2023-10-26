281b28b9e68f76ee4f8514f732cb2d88a26fae3f
Abhinaya148 <83582857+Abhinaya148@users.noreply.github.com>
2023-10-26 23:52:24 +0530

    1

---
+++ Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Int16[] n = { 12, 13, 14 };
+            Console.WriteLine(n.Length);
+
+            string[] course = { "CSE", "IT", "ECE", "EEE","AI" };
+            //Console.WriteLine(course[2]);
+            for (int i = 0; i < course.Length; i++) ;
+            {
+                Console.WriteLine(course[i]);
+            }
+            
+         }
+    }
+}
