ffa10a46af998d1b2f53ab10b74abf4caf94deb0
Abhinaya148 <83582857+Abhinaya148@users.noreply.github.com>
2023-10-26 23:57:17 +0530

    1

---
+++ Delegates/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+delegate int CalculatorDelegate(int a, int b);
+    internal class Program
+    {
+        static int Add(int x, int y)
+        { 
+            return x + y; 
+        }
+        static int Sub(int x, int y) 
+        {
+            return x - y; 
+        }
+        static void Main(string[] args)
+        {
+            CalculatorDelegate addDelegate= Add;
+            CalculatorDelegate subDelegate= Sub;
+            int result1 = addDelegate(20, 7);
+            int result2 = subDelegate(44, 8);
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+        }
+    }
+
