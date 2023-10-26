8f53cc654134aecd7eb27f975b38b900b261f8ee
Abhinaya148 <83582857+Abhinaya148@users.noreply.github.com>
2023-10-27 00:23:34 +0530

    1

---
+++ Strings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Welcome to dot net.";
+            Console.WriteLine(str);
+            char[] chars = { 'W', 'O', 'R', 'D' };
+            string str1=new string(chars);
+            Console.WriteLine(str1);
+            string str2 = new string('c',6);
+            Console.WriteLine(str2);
+
+            StringBuilder sb = new StringBuilder("ABC",20);
+            sb.Append(new char[] {'D','E','F'});
+            sb.AppendFormat("GHI{0}{1}", 'J', 'K');
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
