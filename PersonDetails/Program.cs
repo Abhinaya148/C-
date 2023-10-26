272e7aef1b6ad291a3b346d0da566eb97ca92ade
Abhinaya148 <83582857+Abhinaya148@users.noreply.github.com>
2023-10-26 23:34:41 +0530

    1

---
+++ PersonDetails/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine($"My name is {name}");
+            Console.WriteLine("enter your Branch");
+            string branch = Console.ReadLine();
+            //Console.WriteLine($"I am from {branch} branch");
+            Console.WriteLine("enter your roll no");
+
+            int roll_no = Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine($"i am from {branch} branch. And My roll no is {roll_no}");
+        }
+    }
+}
