129fd0dff9cfa5a8fbfc7e676d7ce3e43551c63c
Abhinaya148 <83582857+Abhinaya148@users.noreply.github.com>
2023-10-27 00:16:24 +0530

    1

---
+++ ObjectInitialiser/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Net.Cache;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectInitialiser
+{
+
+    public class Person
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Person person = new Person { FirstName = "Surabhi", LastName = "Abhinaya",Age = 21};
+          
+            Console.WriteLine($"Name:{person.FirstName} {person.LastName}, Age:{person.Age}");
+        }
+    }
+
+}
