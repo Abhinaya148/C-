291c7f2aaf035b12252c56a45bfdd09108756842
Abhinaya148 <83582857+Abhinaya148@users.noreply.github.com>
2023-10-26 23:50:51 +0530

    1

---
+++ Anonymous/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonymous
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var v = new { Amount = 108, Message = "Hello" };
+            Console.WriteLine(v.Amount+v.Message);
+            var anonArray = new[]
+            {
+                new
+                {
+                    name="apple",diam=4
+                },
+                new
+                {
+                    name="grape",diam=1
+                }
+            };
+        }
+    }
+}
