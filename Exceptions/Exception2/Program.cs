4eb896e91d5228f2b78201845847f79ba68c70e2
Abhinaya148 <83582857+Abhinaya148@users.noreply.github.com>
2023-10-27 00:00:29 +0530

    1

---
+++ Exceptions/Exception2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a , b;
+            int a = 0;
+            try
+            {
+                int b = 45 / a;
+                Console.WriteLine(b);
+            }
+            catch(AttemptToDivideByZero)
+            {
+                Console.WriteLine("");
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine($"{ex}");
+            }   
+        }
+    }
+}
