9c5a650c8a0f7f07f2668b6c2b6132c9ce77eb67
Abhinaya148 <83582857+Abhinaya148@users.noreply.github.com>
2023-10-27 00:15:14 +0530

    1

---
+++ Nullable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nullable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int? TicketsOnSale = 1000;
+            int AvailableTickets;
+            if(TicketsOnSale==null)
+            {
+                AvailableTickets= 0;
+            }
+            else
+            {
+                AvailableTickets = (int)TicketsOnSale;
+            }
+            Console.WriteLine($"AvailableTickets={AvailableTickets}");
+            */
+            int? TicketsonSale = null;
+            int AvailableTickets = TicketsonSale ?? 0;
+            Console.WriteLine($"AvailableTickets={AvailableTickets}");
+        }
+    }
+}
