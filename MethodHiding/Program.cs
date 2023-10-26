8c9bb2ee8345a42c7dd7bf1b15ff4b3c1fb046db
Abhinaya148 <83582857+Abhinaya148@users.noreply.github.com>
2023-10-27 00:12:53 +0530

    1

---
+++ MethodHiding/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodHiding
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Id = 56;
+            student.Name = "Tara";
+            student.Branch = "CSE";
+            string str=student.display();
+            
+            Console.WriteLine(str);
+            string sub = student.Attend("Maths");
+            Console.WriteLine(sub);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodHiding/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodHiding
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Branch { get; set; }
+        
+        //method hiding
+        public string Attend(string sub)
+        {
+            return sub;
+        }
+        public string display()
+        {
+            return  Name + " " + Id + " " + Branch;
+            
+        }
+    }
+}
+
