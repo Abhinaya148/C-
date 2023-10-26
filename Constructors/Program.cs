51beae7383f345d1a5cacc9dd9ddc9ac76057a36
Abhinaya148 <83582857+Abhinaya148@users.noreply.github.com>
2023-10-26 23:45:41 +0530

    1

---
+++ Constructors/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Program
+    {
+        /* public class ParentClass
+
+        {
+            public ParentClass()
+            {
+                Console.WriteLine("Parent class constructor called");
+            }
+            
+        }
+        public class ChildClass: ParentClass
+        {
+            public ChildClass() {
+                Console.WriteLine("Child class constructor called");
+            }
+            
+        }
+        public static void Main(string[] args)
+        {
+            ChildClass c = new ChildClass();
+        }
+        */
+        public class ParentClass
+
+        {
+            public ParentClass()
+            {
+                Console.WriteLine("Parent class constructor called");
+            }
+            public ParentClass(string msg)
+            {
+                Console.WriteLine(msg);
+            }
+
+        }
+        public class ChildClass : ParentClass
+        {
+            public ChildClass() : base("HEllo Abhinaya")
+            {
+                Console.WriteLine("Child class constructor called");
+            }
+
+        }
+        public static void Main(string[] args)
+        {
+            ChildClass c = new ChildClass();
+        }
+    }
+}
