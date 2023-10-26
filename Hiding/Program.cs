1dabe74f078e14857de0c45b1d5c6d12e6c3a8cf
Abhinaya148 <83582857+Abhinaya148@users.noreply.github.com>
2023-10-27 00:08:01 +0530

    1

---
+++ Hiding/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hiding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            {
+                Job j=new Job();
+                roles r=new roles();
+                r.display();
+                j.display();
+            }
+        }
+    }
+}
---
+++ Hiding/job.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hiding
+{
+    public class Job
+    {
+        public virtual void  display()
+        {
+            Console.WriteLine("super  class displaying");
+        }
+            //public int id;
+            //public string name;
+            //public string salary;
+    
+        
+    }
+        
+    
+     class roles : Job 
+    {
+        public override void display()
+    {
+        Console.WriteLine("Sub class this is called");
+    }
+    }
+}
