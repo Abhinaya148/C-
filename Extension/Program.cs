0cf3ec080f82e2cf122b1bec2e09fb3da46d4de0
Abhinaya148 <83582857+Abhinaya148@users.noreply.github.com>
2023-10-27 00:01:38 +0530

    1

---
+++ Extension/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    internal static class MyExtensions
+    {
+        public static int Wordcount(this string str)
+        {
+            return str.Split(new char[] { ' ', '.', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+        public static int call(this string s)
+        {
+            return s.Length;
+
+        }
+        static void Main(string[] args)
+        {
+            string s = "hello wordl";
+            int i = s.Wordcount();
+            Console.WriteLine(i);
+            int length = s.Length;
+            Console.WriteLine(length);
+        }
+    }
+}
+    
+
