038fbcc80eb2b86a3fcbe5c62f352b8c9914167c
Abhinaya148 <83582857+Abhinaya148@users.noreply.github.com>
2023-10-26 23:48:42 +0530

    1

---
+++ Abstract/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Abstract.ast;
+
+namespace Abstract
+{
+    internal class Programz
+    {
+        static void Main(string[] args)
+        {
+            RoadLines roadLines = new RoadLines();
+            Console.WriteLine(roadLines.Mode("Cab"));
+            roadLines.show();
+        }
+    }
+}
