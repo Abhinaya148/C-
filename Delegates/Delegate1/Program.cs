802e338d2c168d8bc262b70f35e968aa69112a09
Abhinaya148 <83582857+Abhinaya148@users.noreply.github.com>
2023-10-26 23:55:49 +0530

    1

---
+++ Delegates/Delegate1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    internal class Program
+    {
+        public delegate void Del(string message);
+        public static void DelegateMethod(string message)
+        {
+            Console.WriteLine(message);
+        }
+        
+        static void Main(string[] args)
+            {
+            Del handler = DelegateMethod;//Instantiating the delegate
+            handler("Hello World");//calling the delegate
+        }
+    }
+}
