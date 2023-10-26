1e8c7c883baf3c061cf49db29623375264d56988
Abhinaya148 <83582857+Abhinaya148@users.noreply.github.com>
2023-10-27 00:17:42 +0530

    1

---
+++ OperatorOverloading/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading
+{
+    internal class Calculator
+    {
+        public int number = 0;
+        public Calculator() 
+        {
+
+        }
+        public Calculator(int n)
+        {
+            number = n;
+        }
+        public static Calculator operator +
+            (Calculator Calc1, Calculator Calc2)
+        {
+            Calculator Calc3 = new Calculator(0);
+            Calc3.number = Calc2.number+Calc1.number;
+            return Calc3;
+        }
+        public void display()
+        {
+            Console.WriteLine($"{number}");
+        }
+    }
+}
---
+++ OperatorOverloading/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator num1= new Calculator(1500);
+            Calculator num2 = new Calculator(2700);
+            Calculator num3 = new Calculator();
+            num3=num1 + num2;
+            num1.display();
+            num2.display();
+            num3.display();
+        }
+    }
+}
