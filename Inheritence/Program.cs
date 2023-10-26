5dab84c779739204e532176a5f634b7cba519fc1
Abhinaya148 <83582857+Abhinaya148@users.noreply.github.com>
2023-10-27 00:09:10 +0530

    1

---
+++ Inheritence/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritence
+{
+        public class Employee
+        {
+            public string FirstName;
+            public string LastName;
+            public string Email;
+            public void PrintFullName() 
+            {
+                Console.WriteLine(FirstName + " " + LastName);
+            }
+        }
+        public class FullTimeEmployee: Employee 
+        {
+            public float YearlySalary;
+        }
+        public class PartTimeEmployee: Employee
+        {
+            public float HourlyRate;
+        }
+    public class program
+    {
+        public static void Main()
+        {
+            FullTimeEmployee FTE= new FullTimeEmployee();
+            FTE.FirstName = "Surabhi";
+            FTE.LastName = "Abhinaya";
+            FTE.YearlySalary = 50000;
+            FTE.PrintFullName();
+
+            PartTimeEmployee PTE= new PartTimeEmployee();
+            PTE.FirstName = "Surabhi";
+            PTE.LastName = "Sravya";
+            PTE.HourlyRate = 4000;
+            PTE.PrintFullName();
+        }
+    }
+    }
+
